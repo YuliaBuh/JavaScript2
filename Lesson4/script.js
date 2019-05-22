a6617f97c281d7bd61ea90bb5091ae0fd7ff1066
YuliaBuh <ulchona90@mail.ru>
2019-05-22 15:51:10 +0500

    Удаляем лишнее

--- Lesson4/script.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const str = document.getElementById("textOne").innerHTML; 
-const regexp = /\B'/g;
-let str2 = str.replace(regexp, '"');
-document.getElementById("textTwo").innerHTML = str2;
-
