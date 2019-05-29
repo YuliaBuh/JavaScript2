31a7c82b54565ade998c7b65c2511fec80f8d6a6
YuliaBuh <ulchona90@mail.ru>
2019-05-29 08:30:26 +0500

    Выполнено второе задание

--- Project/script.js
+++ Project/script.js
@@ -2,7 +2,87 @@
 
 const API = 'https://raw.githubusercontent.com/YuliaBuh/MyProject/master/responses';
 
-class list {
+const app = new Vue({
+  el: '#app',
+  data: {
+    	catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        showCart: false,
+        userSearch: '',
+        imgCart: 'https://placehold.it/50x100',
+        filtered: [],
+        cartItems: [],
+        products: [],
+        imgCatalog: 'https://placehold.it/200x150'
+  },
+   methods: {
+	    getJson(url){
+	    	return fetch(url)
+	            .then(result => result.json())
+	            .catch(error => {
+	                console.log(error)
+	            })
+	    },
+		addProduct(product){
+		    this.getJson(`${API}/addToBasket.json`)
+		        .then(data => {
+		            if(data.result){
+		                let find = this.cartItems.find(el => el.id_product === product.id_product);
+		            if(find){
+		                find.quantity++;
+		            } else {
+		                let prod = Object.assign({quantity: 1}, product);
+		                this.cartItems.push(prod);
+		            }
+		            }
+		     })
+		},
+	    remove(product){
+	        this.getJson(`${API}/deleteFromBasket.json`)
+	            .then(data => {
+	                if(data.result){
+	                    if(product.quantity > 1){
+	                        product.quantity--;
+	                    } else {
+	                        this.cartItems.splice(this.cartItems.indexOf(product), 1);
+	                    }
+	                }
+	            })
+	    },
+	    filter(){
+	        let regExp = new RegExp(this.userSearch, 'i');
+	        this.filtered = this.products.filter(el => regExp.test(el.product_name));
+	   		}
+	 },
+     mounted(){
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for(let el of data.contents){
+                    this.cartItems.push(el);
+                }
+            });
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for(let el of data){
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+            	for(let el of data) {
+	                this.products.push(el);
+	                this.filtered.push(el);
+	            } 
+            });
+    }
+})
+
+
+
+//////////// СТАРОЕ ////////////
+
+/*class list {
 	constructor (url, container) {
 		this.container = container;
 		this.url = url;
@@ -197,7 +277,8 @@ let cart = new Cart();
 let products = new ProductsList(cart);
 setTimeout(() => {
    products.getJson(`getProducts_1.json`).then(data => products.handleData(data));
-}, 300);
+}, 300);*/
+ 
 
 
 
